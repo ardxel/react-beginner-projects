555a87e48ca110da9c23b694a8ba50f628d6b955
ardxel <ardxel@gmail.com>
2023-01-10 19:11:21 +0300

    mission complete

---
+++ 01-birthday-reminder/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import data from "./data";
+import List from "./list";
+function App() {
+    const[people, setPeople] = useState(data);
+
+    return (
+        <main>
+            <section>
+                <h2 class='birthdays-counter'>{people.length} birthdays today</h2>
+                <List people={people}/>
+                <button onClick={() => setPeople([])}>Clear All</button>
+            </section>
+        </main>
+    )
+}
+
+export default App;
