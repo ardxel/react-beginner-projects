cb98dc232d31f850206da9b5989c9d4857a7e058
ardxel <ardxel@gmail.com>
2023-01-19 21:41:31 +0300

    rebuild to tsx

--- 09-color-generator/src/App.tsx
+++ 09-color-generator/src/App.tsx
@@ -1,18 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, {ChangeEvent, FormEvent, useState} from 'react'
 import Colors from './Colors'
-
 import Values from 'values.js'
 
 function App() {
-    const [color, setColor] = useState('');
-    const [list, setList] = useState(new Values('#f15025').all(10));
+    const [color, setColor] = useState<string>('');
+    const [list, setList] = useState<Values[]>(new Values('#f15025').all(10));
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
-        let colors = new Values(color).all(10);
+        let colors: Values[] = new Values(color).all(10);
         setList(colors);
     }
-
+    const handleChangeColor = (e: ChangeEvent<HTMLInputElement>): void => {
+        setColor(e.currentTarget.value);
+    }
+    console.log(list);
     return (
         <>
             <section className='container'>
@@ -22,7 +24,7 @@ function App() {
                         type='text'
                         placeholder='#f15025'
                         className='error'
-                        onChange={(e) => setColor(e.target.value)}/>
+                        onChange={handleChangeColor}/>
                     <button
                         className='btn'
                         type='submit'>submit</button>
--- 09-color-generator/src/utils.ts
+++ 09-color-generator/src/utils.ts
@@ -1,11 +1,10 @@
 
-function componentToHex(c) {
+function componentToHex(c: number): string {
     let hex = c.toString(16);
     return hex.length === 1 ? '0' + hex : hex;
 }
 
-function rgbToHex(r, g, b) {
+export default function rgbToHex(r: number,g: number, b: number): string {
     return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-export default rgbToHex;
